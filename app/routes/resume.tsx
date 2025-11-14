279d562fddc50b55757ab70580453e4461c799fc
Shashank2125 <138694950+Shashank2125@users.noreply.github.com>
2025-11-14 18:51:03 +0530

    Resume feedback components were fixed errors of imports by adding functionality to my utils

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -81,7 +81,7 @@ const Resume = () => {
                         <div className=" flex flex-col gap-8 animate-in fade-in duration-1000">
                              <Summary feedback={feedback}/>
                             {/* we return score if score exist else 0 similarly for suggestion if it exist else */}
-                            <Ats score={feedback.Ats.score || 0} suggestion={feedback.Ats.tips || []}/>
+                            <Ats score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || [] }/>
                             <Details feedback={feedback}/>
                         </div>
                     ):(
